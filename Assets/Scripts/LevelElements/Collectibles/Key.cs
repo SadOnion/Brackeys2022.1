36a143cad42b7d3f27ceaa64fbf889b0b8df8591
konrad.strek <konrad.strek@gmail.com>
2022-02-25 00:12:04 +0100

    Add key particles

--- Assets/Scripts/LevelElements/Collectibles/Key.cs
+++ Assets/Scripts/LevelElements/Collectibles/Key.cs
@@ -6,9 +6,21 @@ public class Key : DefaultCollectible
 {
     [SerializeField] int openedDoorsId;
     [SerializeField] IntEvent onKeyCollected;
+    [SerializeField] ParticleSystem keyCollectParticlesPrefab;
+    
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public override void Collect()
     {
         onKeyCollected.RaiseEvent(openedDoorsId);
+        var keyParticles = Instantiate(keyCollectParticlesPrefab, transform.position, Quaternion.identity);
+        var keyParticlesMain = keyParticles.main;
+        keyParticlesMain.startColor = new ParticleSystem.MinMaxGradient((Color.white + spriteRenderer.color) / 2f,  spriteRenderer.color);
+
     }
 }
