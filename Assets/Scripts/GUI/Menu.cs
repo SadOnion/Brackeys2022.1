1872934da005afa231e49528ef21cdda004025ee
Sad Onion <30446684+SadOnion@users.noreply.github.com>
2022-02-26 20:06:15 +0100

    Menu + HomeDimension

--- Assets/Scripts/GUI/Menu.cs
+++ Assets/Scripts/GUI/Menu.cs
@@ -6,23 +6,22 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] Button playBtn;
-    [SerializeField] Button exitBtn;
-    [SerializeField] CanvasGroup canvasGroup;
-    [SerializeField] LeanTweenType fadeOutType;
+	[SerializeField] Button playBtn;
+	[SerializeField] Button exitBtn;
+	[SerializeField] CanvasGroup canvasGroup;
+	[SerializeField] LeanTweenType fadeOutType;
 
-    public UnityEvent onPlay = new UnityEvent();
 
-    private void Start()
-    {
-        playBtn.onClick.AddListener(Hide);
-        exitBtn.onClick.AddListener(Application.Quit);
-    }
+	private void Start()
+	{
+		playBtn.onClick.AddListener(Hide);
+		exitBtn.onClick.AddListener(Application.Quit);
+	}
 
-    void Hide()
-    {
-        canvasGroup.LeanAlpha(0f, 0.6f)
-            .setEase(fadeOutType)
-            .setOnComplete(() => gameObject.SetActive(false));
-    }
+	void Hide()
+	{
+		canvasGroup.LeanAlpha(0f, 0.6f)
+			.setEase(fadeOutType)
+			.setOnComplete(() => gameObject.SetActive(false));
+	}
 }
