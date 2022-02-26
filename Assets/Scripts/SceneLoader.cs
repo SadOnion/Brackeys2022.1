1b5411ccb373e1e99a19fa43cac83059e7fa58fd
Sad Onion <30446684+SadOnion@users.noreply.github.com>
2022-02-26 17:46:08 +0100

    Audio

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,36 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] SceneField[] scenes;
-    [SerializeField] VoidEvent onSceneFinished;
+	[SerializeField] SceneField[] scenes;
+	[SerializeField] VoidEvent onSceneFinished;
 
-    int currentScene = -1;
+	int currentScene = -1;
 
-    private void Awake()
-    {
-        LoadNextScene();
-    }
+	private void Awake()
+	{
+		LoadNextScene();
+	}
 
-    private void OnEnable()
-    {
-        onSceneFinished.OnEventRaised += LoadNextScene;
-    }
+	private void OnEnable()
+	{
+		onSceneFinished.OnEventRaised += LoadNextScene;
+	}
 
-    private void OnDisable()
-    {
-        onSceneFinished.OnEventRaised -= LoadNextScene;
-    }
+	private void OnDisable()
+	{
+		onSceneFinished.OnEventRaised -= LoadNextScene;
+	}
 
-    void LoadNextScene()
-    {
-        int previousScene = currentScene;
-        currentScene++;
-        if(currentScene < scenes.Length)
-        {
-            LeanTween.cancelAll();
-            SceneManager.LoadScene(scenes[currentScene].sceneName, LoadSceneMode.Additive);
-            if (previousScene >= 0)
-                SceneManager.UnloadSceneAsync(scenes[previousScene].sceneName);
-        }
-    }
+	void LoadNextScene()
+	{
+		int previousScene = currentScene;
+		currentScene++;
+		if (currentScene < scenes.Length)
+		{
+			SceneManager.LoadScene(scenes[currentScene].sceneName, LoadSceneMode.Additive);
+			if (previousScene >= 0)
+				SceneManager.UnloadSceneAsync(scenes[previousScene].sceneName);
+		}
+	}
 }
